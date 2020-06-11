8babb7219c1fc48dd39f4eb25b2663b9ce35849c
ESbros <edsg77@gmail.com>
2020-06-10 20:44:37 -0500

    new_columns

--- Final_Zoho/connect_create_insert.py
+++ Final_Zoho/connect_create_insert.py
@@ -56,10 +56,11 @@ def create_db(db_name, db_host, db_user, db_pass):
                     Dias_Inactivos VARCHAR(255), Promedias_entre_compras VARCHAR(255), \
                     Dias_de_Pago_2 VARCHAR(255), Promedio_dias_de_vencimiento VARCHAR(255), \
                     Dias_factura_vencida_mora VARCHAR(255), Saldo_Pendiente VARCHAR(255), \
-                    Shipping_City VARCHAR(255), \
+                    Shipping_City VARCHAR(255), Correo_Electronico_Fact VARCHAR(255),\
                     Transporte_preferido VARCHAR(255), Shipping_Street VARCHAR(255), \
-                    Rating VARCHAR(255), Shipping_State VARCHAR(255), \
-                    Phone VARCHAR(255), Account_Name VARCHAR(255), RUC VARCHAR(255))")
+                    Descripcion VARCHAR(255),\
+                    Rating VARCHAR(255), Shipping_State VARCHAR(255), Website VARCHAR(255),\
+                    Correo_electronico VARCHAR(255),Phone VARCHAR(255), Account_Name VARCHAR(255), RUC VARCHAR(255))")
 
     #Show Tables
     mycursor.execute("SHOW TABLES")
@@ -95,34 +96,32 @@ def consult_insert(db_name, db_host, db_user, db_pass):
                 Fecha_Ult_Comp, Limite_de_Credito, Industry, Estado_del_Cliente, \
                 Promedio_de_Ventas_Mensual, Dias_Inactivos, Promedias_entre_compras, \
                 Dias_de_Pago_2, Promedio_dias_de_vencimiento, Dias_factura_vencida_mora, \
-                Saldo_Pendiente, Shipping_City, Transporte_preferido, Shipping_Street, \
-                Rating, Shipping_State, Phone, Account_Name, RUC) \
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                Saldo_Pendiente, Shipping_City, \
+                Correo_Electronico_Fact, \
+                Transporte_preferido, Shipping_Street, \
+                Descripcion,  Rating,\
+                Shipping_State, \
+                Website, Correo_electronico,\
+                Phone, Account_Name, RUC) \
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
     record_ins_arr = resp.data
-    referece_ins = {}
-    for ins in record_ins_arr:
-        if len(ins.field_data) == 21:
-            referece_ins = ins.field_data
-
-    campos_cliente = ['entity_id', 'owner_id']
-    for key in referece_ins:
-        if key != 'L_nea_de_Negocio' and key != 'Tag':
-            campos_cliente.append(key)
+    campos_cliente = ['entity_id', 'owner_id', 'Fecha_ltima_compra1', 'L_mite_de_Cr_dito', 'Industry', 'Estado_del_Cliente', 'Promedio_de_Ventas_Mensual', 'D_as_Inactivos', 'Promedio_d_as_entre_compras', 'D_as_de_Pago_2', 'Promedio_de_D_as_de_vencimiento', 'D_as_factura_vencida_mora', 'Saldo_Pendiente', 'Shipping_City', 'Correo_electr_nico_Facturaci_n', 'Transporte_preferido', 'Shipping_Street', 'Description', 'Rating', 'Shipping_State', 'Website', 'Correo_electr_nico', 'Phone', 'Account_Name', 'RUC']
+    print(len(campos_cliente))
 
     for record_ins in record_ins_arr:
         cliente = (record_ins.entity_id, record_ins.owner.id)
         product = []
         product_data = record_ins.field_data
-        for key in campos_cliente:
-            if key != 'entity_id' and key != 'owner_id':
-                try:
-                    product.append(str(product_data[key]))
-                except:
-                    product.append('null')
+        for column in campos_cliente:
+            value = 'null'
+            if column != 'entity_id' and column != 'owner_id':
+                for key in product_data:
+                    if key == column:
+                        value = product_data[key]
+                product.append(str(value))
         cliente = cliente + tuple(product)        
         print(cliente)
-
         mycursor.execute(sqlFormula, cliente)
         mydb.commit()
 
